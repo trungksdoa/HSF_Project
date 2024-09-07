4106690b9ed78d6d68f21f644e73fc72ec2775c2
Võ Hoàng Trung <55863018+trungksdoa@users.noreply.github.com>
2024-09-07 10:07:52 +0700

    Initial commit

---
+++ src/main/java/com/product/server/koi_hsf/KoiHsfApplication.java
@@ -0,0 +1,13 @@
+package com.product.server.koi_hsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoiHsfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoiHsfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/product/server/koi_hsf/KoiHsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.server.koi_hsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoiHsfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
